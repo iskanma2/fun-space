4cb243b172915a95eb541d9b7f305422a5fc85cf
USERNAME AKUN GITHUB <iskmaulana@gmail.com>
2024-03-05 22:47:13 +0700

    First Github Project

---
+++ app.py
@@ -0,0 +1,8 @@
+# Hitung LUas Segitiga sederhana
+def luas_segitiga():
+    a = int(input("masukan alas segitiga: "))
+    t = int(input("MAsukan tinggi segitiga: "))
+    luas = a * t / 2
+    print ("Luas segitiga adalah: ", luas)
+
+luas_segitiga()
